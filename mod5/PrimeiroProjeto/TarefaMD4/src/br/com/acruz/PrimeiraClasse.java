119d6043af36f33c72083243880271baf031168c
AdsonCruz <Adsoncruzzproc@gmail.com>
2024-05-12 13:03:18 -0300

    alteçao no codigo

--- mod5/PrimeiroProjeto/TarefaMD4/src/br/com/acruz/PrimeiraClasse.java
+++ mod5/PrimeiroProjeto/TarefaMD4/src/br/com/acruz/PrimeiraClasse.java
@@ -2,6 +2,6 @@ package br.com.acruz;
 
 public class PrimeiraClasse {
     public static void main(String args[]) {
-        System.out.print("Hello");
+        System.out.print("Hello Adson Cruz");
     }
 }
