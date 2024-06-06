17fa7110ace31e163bc13bba47d8ccb5a299fe65
AdsonCruz <Adsoncruzzproc@gmail.com>
2024-06-06 20:42:22 -0300

    Exercicio módulo 9

---
+++ TarefaWrapper/br/com/acruz/Principal.java
@@ -0,0 +1,10 @@
+package br.com.acruz;
+
+public class Principal {
+    public static void main(String[] args) {
+        int numero = 10;
+        System.out.println(numero);
+        int num2 = (Integer) numero;
+        System.out.println(num2);
+    }
+}
