12c324955b53f21b6f1cb138203a932902535a6a
AdsonCruz <Adsoncruzzproc@gmail.com>
2024-08-13 02:36:38 -0300

    Atualizacao Tarefa do Mod 13

--- Tarefamod13/br/com/acruz/pessoafisica.java
+++ Tarefamod13/br/com/acruz/pessoafisica.java
@@ -1,7 +1,32 @@
 package br.com.acruz;
+public class pessoaFisica extends Pessoa{
+    private String cpf;
+    private String dataDenascimento;
 
-public abstract class pessoafisica {
+    public  pessoaFisica(String nome, String endereco, String telefone){
+        super(nome,endereco,telefone);
+        this.cpf = cpf;
+        this.dataDenascimento = dataDenascimento;
+    }
 
-    public String nome;
-    public int idade;
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataDenascimento() {
+        return dataDenascimento;
+    }
+
+    public void setDataDenascimento(String dataDenascimento) {
+        this.dataDenascimento = dataDenascimento;
+    }
 }
+
+
+
+
+
--- Tarefamod13/br/com/acruz/pessoajuridica.java
+++ Tarefamod13/br/com/acruz/pessoajuridica.java
@@ -1,8 +1,28 @@
 package br.com.acruz;
 
-public class pessoajuridica extends pessoafisica{
-    public int cnpj() {
-        return 0;
+public class pessoaJuridica extends Pessoa{
+    private String cnpj;
+    private String dataInalguracao;
+
+    public pessoaJuridica(String nome, String endereco, String telefone){
+        super(nome, endereco, telefone);
+        this.cnpj = cnpj;
+        this.dataInalguracao = dataInalguracao;
+
+    }
+    public String getCnpj() {
+        return cnpj;
     }
 
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getDataInalguracao() {
+        return dataInalguracao;
+    }
+
+    public void setDataInalguracao(String dataInalguracao) {
+        this.dataInalguracao = dataInalguracao;
+    }
 }
